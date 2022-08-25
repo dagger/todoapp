25e5e8307a0d43ced5073754f092cfde0cfe76ae
Solomon Hykes <solomon@dagger.io>
2022-08-24 23:19:16 -1000

    First successful run of build script in cloak
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- workflows/build/index.mjs
+++ workflows/build/index.mjs
@@ -4,8 +4,8 @@ import { gql, Engine } from "@dagger.io/dagger";
 new Engine({
 	ConfigPath: process.env.CLOAK_CONFIG
 }).run(async (client) => {
-  // 1. Load app source code from working directory
-  const source = await client.request(gql`
+	// 1. Load app source code from working directory
+	const source = await client.request(gql`
 	{
 	  host {
 	    workdir {
@@ -17,8 +17,8 @@ new Engine({
 	}
   `).then((result) => result.host.workdir.read)
 
-  // 2. Install yarn in a container
-  const image = await client.request(gql`
+	// 2. Install yarn in a container
+	const image = await client.request(gql`
 	{
 	  core {
 	    image(ref:"index.docker.io/alpine") {
@@ -30,7 +30,11 @@ new Engine({
 		  exec(input:{args:["apk", "add", "git"]}) {
 		    stdout
 		    fs {
-		      id
+			  exec(input: {args: ["apk", "add", "openssh"]}) {
+				fs {
+				  id
+				}
+			  }
 		    }
 		  }
 		}
@@ -38,11 +42,60 @@ new Engine({
 	    }
 	  }
 	}
-`).then((result) => result.core.image.exec.fs.exec.fs)
+`).then((result) => result.core.image.exec.fs.exec.fs.exec.fs)
 
-  // 3. Run 'yarn install' in a container
+	// 3. Run 'yarn install' in a container
+	const sourceAfterInstall = await client.request(gql`
+	{
+		core {
+		  filesystem(id: "${image.id}") {
+			exec(
+			  input: {
+				args: ["yarn", "install"]
+				mounts: [{ fs: "${source.id}", path: "/src" }]
+				workdir: "/src"
+			  }
+			) {
+			  # Retrieve modified source
+			  mount(path: "/src") {
+				id
+			  }
+			}
+		  }
+		}
+	  }
+	`).then((result) => result.core.filesystem.exec.mount)
 
-  // 4. Run 'yarn run build' in a container
+	// 4. Run 'yarn run build' in a container
+	const sourceAfterBuild = await client.request(gql` 
+	{
+		core {
+		  filesystem(id: "${image.id}") {
+			exec(
+			  input: {
+				args: ["./node_modules/.bin/react-scripts", "build"]
+				mounts: [{ fs: "${sourceAfterInstall.id}", path: "/src" }]
+				workdir: "/src"
+			  }
+			) {
+			  # Retrieve modified source
+			  mount(path: "/src") {
+				id
+			  }
+			}
+		  }
+		}
+	  }
+	  `).then((result) => result.core.filesystem.exec.mount)
 
-  // 5. write the result back to workdir
+	// 5. write the result back to workdir
+	const result = await client.request(gql`
+	{
+		host {
+			workdir {
+				write(contents: "${sourceAfterBuild.id}")
+			}
+		}
+	}
+	`)
 });
