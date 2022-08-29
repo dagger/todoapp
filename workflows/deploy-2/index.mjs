7838e07858f05fe03c6bd7f5d7d1c9dc7030ef68
Andrea Luzzardi <aluzzardi@gmail.com>
2022-08-29 20:39:06 +0200

    workflows/deploy-2: run prettier
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- workflows/deploy-2/index.mjs
+++ workflows/deploy-2/index.mjs
@@ -3,45 +3,54 @@ import { gql, Engine } from "@dagger.io/dagger";
 
 // Check for netlify token
 if (!process.env.NETLIFY_AUTH_TOKEN) {
-	console.log("Missing netlify API token. Please set it to the env variable $NETLIFY_AUTH_TOKEN")
-	process.exit(1)
+  console.log(
+    "Missing netlify API token. Please set it to the env variable $NETLIFY_AUTH_TOKEN"
+  );
+  process.exit(1);
 }
 
 // Set default value if NETLIFY_SITE_NAME is unset
-const netlifySiteName = process.env.NETLIFY_SITE_NAME ?? `${process.env.USER}-dagger-todoapp`
+const netlifySiteName =
+  process.env.NETLIFY_SITE_NAME ?? `${process.env.USER}-dagger-todoapp`;
 
-console.log(process.env.NETLIFY_SITE_NAME 
-	? `
+console.log(
+  process.env.NETLIFY_SITE_NAME
+    ? `
 		Using netlify site name: "${netlifySiteName}"
 	`
-	: `
+    : `
 		Netlify site name not specified in $NETLIFY_SITE_NAME.
 		Defaulting to "${netlifySiteName}".
 	`
-)
+);
 
-const netlifyTokenCleartext = process.env.NETLIFY_AUTH_TOKEN
+const netlifyTokenCleartext = process.env.NETLIFY_AUTH_TOKEN;
 
 // Start cloak engine
 new Engine({
-	ConfigPath: process.env.CLOAK_CONFIG
+  ConfigPath: process.env.CLOAK_CONFIG,
 }).run(async (client) => {
-	
-	// 1. Load app source code from working directory
-	const source = await client.request(gql`
-	{
-	  host {
-	    workdir {
-	      read {
-	        id
-	      }
-	    }
-	  }
-	}
-  `).then((result) => result.host.workdir.read)
+  // 1. Load app source code from working directory
+  const source = await client
+    .request(
+      gql`
+        {
+          host {
+            workdir {
+              read {
+                id
+              }
+            }
+          }
+        }
+      `
+    )
+    .then((result) => result.host.workdir.read);
 
-	// 2. Build the app with yarn
-	const sourceAfterBuild = await client.request(gql`
+  // 2. Build the app with yarn
+  const sourceAfterBuild = await client
+    .request(
+      gql`
 	{
 	  yarn {
 		script(source: "${source.id}", runArgs: ["build"]) {
@@ -49,33 +58,38 @@ new Engine({
 		}
 	  }
 	}
-	`).then((result) => result.yarn.script)
+	`
+    )
+    .then((result) => result.yarn.script);
 
-	// 3. Load netlify token into a secret
-	const netlifyTokenSecret = await client.request(gql`
+  // 3. Load netlify token into a secret
+  const netlifyTokenSecret = await client
+    .request(
+      gql`
 	{
 		core {
 			addSecret(plaintext: "${netlifyTokenCleartext}")
 		}
 	}
-	`).then((result) => result.core.addSecret)
+	`
+    )
+    .then((result) => result.core.addSecret);
 
-	// 4. deploy to Netlify
-	const result = await client.request(gql`
+  // 4. deploy to Netlify
+  const result = await client.request(gql`
 	{
 		netlify {
 			deploy(
 				contents: "${sourceAfterBuild.id}",
 				subdir: "build",
 				siteName: "${netlifySiteName}",
-				token: "${netlifyTokenSecret
-			}") {
+				token: "${netlifyTokenSecret}") {
 				url
 				deployURL
 			}
 		}
 	}
-	`)
+	`);
 
-	console.log("Netlify deploy URL: " + result.netlify.deploy.url)
+  console.log("Netlify deploy URL: " + result.netlify.deploy.url);
 });
