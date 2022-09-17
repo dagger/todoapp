72719d085623267d2cd6fb196ddebc3ebe9ec956
Erik Sipsma <erik@sipsma.dev>
2022-09-16 17:52:14 -0700

    Upgrade to new dagger dependencies.
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- backend/magefiles/main.go
+++ backend/magefiles/main.go
@@ -7,14 +7,18 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/dagger/cloak/engine"
-	"github.com/dagger/cloak/sdk/go/dagger"
-	"github.com/dagger/todoapp/api/magefiles/gen/core"
-	"github.com/dagger/todoapp/api/magefiles/gen/todoapp"
+	"github.com/Khan/genqlient/graphql"
+	"go.dagger.io/dagger/engine"
+	"go.dagger.io/dagger/sdk/go/dagger"
 )
 
 func Deploy(ctx context.Context) {
 	if err := engine.Start(ctx, &engine.Config{}, func(ctx engine.Context) error {
+		client, err := dagger.Client(ctx)
+		if err != nil {
+			return err
+		}
+
 		// User can configure netlify site name with $NETLIFY_SITE_NAME
 		siteName, ok := os.LookupEnv("NETLIFY_SITE_NAME")
 		if !ok {
@@ -30,32 +34,76 @@ func Deploy(ctx context.Context) {
 		}
 
 		// Load API token into a secret
-		var token dagger.SecretID
-		if resp, err := core.AddSecret(ctx, tokenCleartext); err != nil {
+		token, err := addSecret(ctx, client, tokenCleartext)
+		if err != nil {
 			return err
-		} else {
-			token = resp.Core.AddSecret
-		}
-
-		// Load source code from workdir
-		var source dagger.FSID
-		if resp, err := core.Workdir(ctx); err != nil {
-			return err
-		} else {
-			source = resp.Host.Workdir.Read.ID
 		}
 
 		// Deploy using the todoapp deploy extension
-		resp, err := todoapp.Deploy(ctx, source, siteName, token)
+		url, err := deploy(ctx, client, ctx.Workdir, siteName, token)
 		if err != nil {
 			return err
 		}
 
 		// Print deployment info to the user
-		fmt.Println("URL:", resp.Todoapp.Deploy)
+		fmt.Println("URL:", url)
 
 		return nil
 	}); err != nil {
 		panic(err)
 	}
+}
+
+func addSecret(ctx context.Context, client graphql.Client, plaintext string) (dagger.SecretID, error) {
+	req := &graphql.Request{
+		Query: `
+query AddSecret ($plaintext: String!) {
+	core {
+		addSecret(plaintext: $plaintext)
+	}
+}
+`,
+		Variables: map[string]any{
+			"plaintext": plaintext,
+		},
+	}
+	resp := struct {
+		Core struct {
+			AddSecret dagger.SecretID
+		}
+	}{}
+	err := client.MakeRequest(ctx, req, &graphql.Response{Data: &resp})
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Core.AddSecret, nil
+}
+
+func deploy(ctx context.Context, client graphql.Client, fs dagger.FSID, siteName string, token dagger.SecretID) (string, error) {
+	req := &graphql.Request{
+		Query: `
+query Deploy ($fs: FSID!, $siteName: String!, $token: SecretID!) {
+	todoapp {
+		deploy(source: $fs, siteName: $siteName, token: $token)
+	}
+}
+`,
+		Variables: map[string]any{
+			"fs":       fs,
+			"siteName": siteName,
+			"token":    token,
+		},
+	}
+	resp := struct {
+		Todoapp struct {
+			Deploy string
+		}
+	}{}
+	err := client.MakeRequest(ctx, req, &graphql.Response{Data: &resp})
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Todoapp.Deploy, nil
 }
